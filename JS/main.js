bf81c04120bcd9dbd120b11641816e5d1958627d
Micah Ellis <micahsellis@gmail.com>
2020-08-10 14:24:00 -0600

    add handleClick function

--- JS/main.js
+++ JS/main.js
@@ -16,31 +16,32 @@ let winner;
 /*----- cached element references -----*/
 const playerTurnEl = document.getElementById("playerTurn");
 const allColumnEls = document.querySelectorAll(".circle0, .circle1, .circle2, .circle3, .circle4, .circle5, .circle6");
-const column0Els = document.querySelectorAll(".circle0");
-const column1Els = document.querySelectorAll(".circle1");
-const column2Els = document.querySelectorAll(".circle2");
-const column3Els = document.querySelectorAll(".circle3");
-const column4Els = document.querySelectorAll(".circle4");
-const column5Els = document.querySelectorAll(".circle5");
-const column6Els = document.querySelectorAll(".circle6");
+const column0Els = document.querySelectorAll(".c0");
+const column1Els = document.querySelectorAll(".c1");
+const column2Els = document.querySelectorAll(".c2");
+const column3Els = document.querySelectorAll(".c3");
+const column4Els = document.querySelectorAll(".c4");
+const column5Els = document.querySelectorAll(".c5");
+const column6Els = document.querySelectorAll(".c6");
 const gaemStateEl = document.getElementById("gameState");
 
 
 
 /*----- event listeners -----*/
-for (i of column0Els) { i.addEventListener('click', handleColumn0Click) };
-for (i of column1Els) { i.addEventListener('click', handleColumn1Click) };
-for (i of column2Els) { i.addEventListener('click', handleColumn2Click) };
-for (i of column3Els) { i.addEventListener('click', handleColumn3Click) };
-for (i of column4Els) { i.addEventListener('click', handleColumn4Click) };
-for (i of column5Els) { i.addEventListener('click', handleColumn5Click) };
-for (i of column6Els) { i.addEventListener('click', handleColumn6Click) };
+for (i of column0Els) { i.addEventListener('click', handleClick) };
+for (i of column1Els) { i.addEventListener('click', handleClick) };
+for (i of column2Els) { i.addEventListener('click', handleClick) };
+for (i of column3Els) { i.addEventListener('click', handleClick) };
+for (i of column4Els) { i.addEventListener('click', handleClick) };
+for (i of column5Els) { i.addEventListener('click', handleClick) };
+for (i of column6Els) { i.addEventListener('click', handleClick) };
 document.getElementById('resetbtn').addEventListener('click', eraseBoard);
 
 
 /*----- functions -----*/
 function init() {
     board = {
+        c0: [null, null, null, null, null, null, null],
         c1: [null, null, null, null, null, null, null],
         c2: [null, null, null, null, null, null, null],
         c3: [null, null, null, null, null, null, null],
@@ -53,27 +54,25 @@ function init() {
     gaemStateEl.textContent = "MAKE YOUR MOVE!";
     render();
 }
-function handleColumn0Click() {
-    console.log('Column 0');
+function handleClick(evt) {
+    const playerRow = parseInt(evt.target.id);
+    const playerColumn = evt.target.className;
+    if (!board[playerColumn].includes(null)) return;
+    let idx = board[playerColumn].findIndex((e) => e === null);
+    board[playerColumn].splice(idx, 1, turn);
+    if (turn === -1) {
+        player2Go();
+    }
+    if (turn === 1) {
+        player1Go();
+    }
 };
-function handleColumn1Click() {
-    console.log('Column 1');
-};
-function handleColumn2Click() {
-    console.log('Column 2');
-};
-function handleColumn3Click() {
-    console.log('Column 3');
-};
-function handleColumn4Click() {
-    console.log('Column 4');
-};
-function handleColumn5Click() {
-    console.log('Column 5');
-};
-function handleColumn6Click() {
-    console.log('Column 6');
-};
+function player2Go() {
+    console.log('P2!')
+}
+function player1Go() {
+    console.log('P1!')
+}
 function isGameOver() {
     console.log('game still in play');
 }
