006ca6a6ccef7712553b87ca24856017a6f529d2
Micah Ellis <micahsellis@gmail.com>
2020-08-10 17:22:21 -0600

    add style changes

--- JS/main.js
+++ JS/main.js
@@ -3,9 +3,13 @@ const PLAYER = {
   p1: 1,
   p1Color: 'rgba(255,0,115,1)',
   p1Glow: '0 0 10px 5px rgba(255,0,115,0.8)',
+  p1TxtShd:
+    '1px 1px 2px black, 0 0 20px rgba(92, 255, 230, 0.6), 0 0 20px rgba(92, 255, 240, 0.5), 0 0 20px rgba(92, 255, 255, 0.7)',
   p2: -1,
   p2Color: 'rgba(92,225,230,1)',
   p2Glow: '0 0 10px 5px rgba(92,255,230,0.8)',
+  p2TxtShd:
+    '1px 1px 2px black, 0 0 20px rgba(255, 0, 115, 1), 0 0 20px rgba(255, 0, 100, 1), 0 0 20px rgba(255, 0, 90, 1)',
 };
 
 /*----- app's state (variables) -----*/
@@ -30,13 +34,13 @@ document.getElementById('resetbtn').addEventListener('click', eraseBoard);
 /*----- functions -----*/
 function init() {
   board = {
-    c0: [null, null, null, null, null, null, null],
-    c1: [null, null, null, null, null, null, null],
-    c2: [null, null, null, null, null, null, null],
-    c3: [null, null, null, null, null, null, null],
-    c4: [null, null, null, null, null, null, null],
-    c5: [null, null, null, null, null, null, null],
-    c6: [null, null, null, null, null, null, null],
+    c0: [null, null, null, null, null, null],
+    c1: [null, null, null, null, null, null],
+    c2: [null, null, null, null, null, null],
+    c3: [null, null, null, null, null, null],
+    c4: [null, null, null, null, null, null],
+    c5: [null, null, null, null, null, null],
+    c6: [null, null, null, null, null, null],
   };
   turn = 1;
   winner = null;
@@ -55,9 +59,9 @@ function handleClick(evt) {
   if (turn === 1) {
     player1Go(playerColumn, idx);
   }
-    winner = isGameOver();
-    turn *= -1;
-    render(turn);
+  winner = isGameOver();
+  turn *= -1;
+  render(turn);
 }
 function player2Go(playerColumn, idx) {
   let columnNum = playerColumn.substr(1);
@@ -76,27 +80,32 @@ function player1Go(playerColumn, idx) {
   clicked.removeEventListener('click', handleClick);
 }
 function isGameOver() {
-    console.log('game still in play');
-    return null;
+  console.log('game still in play');
+  return null;
 }
 function render(turn) {
-    if (winner === null) {
-        if (turn === 1) {
-            playerEl.src = "imgs/Player1.png";
-            gameStateEl.style.color = PLAYER.p1Color;
-        } else if (turn === -1) {
-            playerEl.src = "imgs/Player2.png";
-            gameStateEl.style.color = PLAYER.p2Color;
-      }
-    } else if (winner === 'T') {
-        gameStateEl.textContent = "IT'S A TIE!"
-        gameStateEl.style.color = "black";
-    } else if (winner === 1) {
-        gameStateEl.textContent = "PLAYER 1 WINS!"
-        gameStateEl.style.color = PLAYER.p1Color
-    } else if (winner === -1) {
-        gameStateEl.textContent = "PLAYER 2 WINS!"
-        gameStateEl.style.color = PLAYER.p2Color
+  if (winner === null) {
+    if (turn === 1) {
+      playerEl.src = 'imgs/Player1.png';
+      gameStateEl.style.color = PLAYER.p1Color;
+      gameStateEl.style.textShadow = PLAYER.p1TxtShd;
+    } else if (turn === -1) {
+      playerEl.src = 'imgs/Player2.png';
+      gameStateEl.style.color = PLAYER.p2Color;
+      gameStateEl.style.textShadow = PLAYER.p2TxtShd;
+    }
+  } else if (winner === 'T') {
+    gameStateEl.textContent = "IT'S A TIE!";
+    gameStateEl.style.color = 'black';
+    gameStateEl.style.textShadow = '0 0 20px white, 0 0 20px white';
+  } else if (winner === 1) {
+    gameStateEl.textContent = 'PLAYER 1 WINS!';
+    gameStateEl.style.color = PLAYER.p1Color;
+    gameStateEl.style.textShadow = PLAYER.p1TxtShd;
+  } else if (winner === -1) {
+    gameStateEl.textContent = 'PLAYER 2 WINS!';
+    gameStateEl.style.color = PLAYER.p2Color;
+    gameStateEl.style.textShadow = PLAYER.p2TxtShd;
   }
 }
 function eraseBoard() {
