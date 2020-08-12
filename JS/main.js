b9d05a0792032b6ba83913043b6217ecc672b88f
Micah Ellis <micahsellis@gmail.com>
2020-08-12 10:20:24 -0600

    add row and diagonal logic

--- JS/main.js
+++ JS/main.js
@@ -26,9 +26,6 @@ const gameStateEl = document.getElementById('gameState');
 const playerEl = document.getElementById('player-turn');
 
 /*----- event listeners -----*/
-for (i of allColumnEls) {
-  i.addEventListener('click', handleClick);
-}
 document.getElementById('resetbtn').addEventListener('click', eraseBoard);
 
 /*----- functions -----*/
@@ -42,6 +39,9 @@ function init() {
     c5: [null, null, null, null, null, null],
     c6: [null, null, null, null, null, null],
   };
+  for (i of allColumnEls) {
+    i.addEventListener('click', handleClick);
+  }
   turn = 1;
   gameStateEl.textContent = 'MAKE YOUR MOVE!';
   render();
@@ -81,91 +81,100 @@ function player1Go(playerColumn, idx) {
   clicked.style.boxShadow = PLAYER.p1Glow;
   clicked.removeEventListener('click', handleClick);
 }
-/*
-  board = {
-    c0: [null, null, null, null, null, null],
-    c1: [null, null, null, null, null, null],
-    c2: [null, null, null, null, null, null],
-    c3: [null, null, null, null, null, null],
-    c4: [null, null, null, null, null, null],
-    c5: [null, null, null, null, null, null],
-    c6: [null, null, null, null, null, null],
-  };
-  */ 
+
 function isGameOver() {
-  // for (const [key, value] of Object.entries(board)) {
+  // check columns
   Object.entries(board).forEach(([key, value]) => {
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < 4; i++) {
       if (
         board[key][i] === 1 &&
         board[key][i + 1] === 1 &&
         board[key][i + 2] === 1 &&
         board[key][i + 3] === 1
       ) {
-        console.log('winner is 1');
         winner = board[key][i];
-        console.log(winner)
-        return winner
+        return winner;
       } else if (
         board[key][i] === -1 &&
         board[key][i + 1] === -1 &&
         board[key][i + 2] === -1 &&
         board[key][i + 3] === -1
       ) {
-        console.log('winner is -1');
-        return board[key][i];
-      } else return null
+        winner = board[key][i];
+        return winner;
+      }
     }
-  })
-};
-
-
-                        // if (board.c0[0] === board.c0[1] )
-                        //check Verticle
-                        //   for (i = 0; i < 3; i++) {
-                        //     Object.keys()
-                        //   }
-                        // }
-                        // function chkWinner(board) {
-                        //   // Check down
-                        //   for (r = 0; r < 3; r++)
-                        //     Object.entries(board).forEach(([key, value]) => {
-                        //       if chkLine(key )
-                        //     })
-                        //     for (c = 0; c < 7; c++)
-                        //       if (chkLine(board., bd[r + 1][c], bd[r + 2][c], bd[r + 3][c]))
-                        //         return bd[r][c];
-                        // Object.entries(animals).forEach(([key, value]) => {
-                        //     console.log(`${key}: ${value}`)
-                        // });
-
-                        // Object.entries & Object.keys
-                        //   // Check right
-                        //   for (r = 0; r < 6; r++)
-                        //     for (c = 0; c < 4; c++)
-                        //       if (chkLine(bd[r][c], bd[r][c + 1], bd[r][c + 2], bd[r][c + 3]))
-                        //         return bd[r][c];
-
-                        //   // Check down-right
-                        //   for (r = 0; r < 3; r++)
-                        //     for (c = 0; c < 4; c++)
-                        //       if (
-                        //         chkLine(bd[r][c], bd[r + 1][c + 1], bd[r + 2][c + 2], bd[r + 3][c + 3])
-                        //       )
-                        //         return bd[r][c];
-
-                        //   // Check down-left
-                        //   for (r = 3; r < 6; r++)
-                        //     for (c = 0; c < 4; c++)
-                        //       if (
-                        //         chkLine(bd[r][c], bd[r - 1][c + 1], bd[r - 2][c + 2], bd[r - 3][c + 3])
-                        //       )
-                        //         return bd[r][c];
-
-                        //   return 0;
-                        // }
-                        // console.log(board);
-                      
+  });
+  // convert board object's values to arrays
+  let boardArr = Object.values(board);
+  // check rows
+  for (c = 0; c < 5; c++) {
+    for (r = 0; r < 7; r++) {
+      if (
+        boardArr[c][r] === 1 &&
+        boardArr[c + 1][r] === 1 &&
+        boardArr[c + 2][r] === 1 &&
+        boardArr[c + 3][r] === 1
+      ) {
+        winner = boardArr[c][r];
+        return winner;
+      } else if (
+        boardArr[c][r] === -1 &&
+        boardArr[c + 1][r] === -1 &&
+        boardArr[c + 2][r] === -1 &&
+        boardArr[c + 3][r] === -1
+      ) {
+        winner = boardArr[c][r];
+        return winner;
+      }
+    }
+  }
+  // check diagonal right
+  for (c = 0; c < 4; c++) {
+    for (r = 0; r < 3; r++) {
+      if (
+        boardArr[c][r] === 1 &&
+        boardArr[c + 1][r + 1] === 1 &&
+        boardArr[c + 2][r + 2] === 1 &&
+        boardArr[c + 3][r + 3] === 1
+      ) {
+        winner = boardArr[c][r];
+        return winner;
+      } else if (
+        boardArr[c][r] === -1 &&
+        boardArr[c + 1][r + 1] === -1 &&
+        boardArr[c + 2][r + 2] === -1 &&
+        boardArr[c + 3][r + 3] === -1
+      ) {
+        winner = boardArr[c][r];
+        return winner;
+      }
+    }
+  }
+  // check diagonal left
+  for (c = 0; c < 4; c++) {
+    for (r = 3; r < 6; r++) {
+      if (
+        boardArr[c][r] === 1 &&
+        boardArr[c + 1][r - 1] === 1 &&
+        boardArr[c + 2][r - 2] === 1 &&
+        boardArr[c + 3][r - 3] === 1
+      ) {
+        winner = boardArr[c][r];
+        return winner;
+      } else if (
+        boardArr[c][r] === -1 &&
+        boardArr[c + 1][r - 1] === -1 &&
+        boardArr[c + 2][r - 2] === -1 &&
+        boardArr[c + 3][r - 3] === -1
+      ) {
+        winner = boardArr[c][r];
+        return winner;
+      }
+    }
+  }
+  return null;
+}
 
 function render(turn) {
   if (winner === null) {
